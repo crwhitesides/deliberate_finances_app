ca6c0e897370a32e1e79f8b4b50abe8126dbd0fd
Christian Whitesides <whitesidescr@gmail.com>
2016-09-20 16:10:38 -0600

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ActiveRecord::Base
   has_many :plans, through: :user_plans
   has_many :comments
 
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
