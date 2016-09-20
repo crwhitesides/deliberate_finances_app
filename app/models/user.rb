dff2bdbd9089eb708280b77759e2812252679275
Christian Whitesides <whitesidescr@gmail.com>
2016-09-20 16:21:10 -0600

    Update password vallidation to only check length

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 end
