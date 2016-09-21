999be04a3a20dd24b70c0ad816e8ef3463ebb50a
Christian Whitesides <whitesidescr@gmail.com>
2016-09-21 15:57:03 -0600

    Add to additional helper methods to validate user log-in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,12 @@ module SessionsHelper
   def current_user
     @user ||= user.find_by(id: session[:user_id])
   end
+
+  def logged_in?
+    current_user.id != nil
+  end
+
+  def require_logged_in?
+    return redirect_to login_path unless logged_in?
+  end
 end
