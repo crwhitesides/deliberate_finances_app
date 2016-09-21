55618a131a1fc36b01e4bc68da102c025677803a
Christian Whitesides <whitesidescr@gmail.com>
2016-09-21 16:31:29 -0600

    Finish user logout
    - Add log_out helper method in SessionsHelper
    - Finish destroy action in SessionsController

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete :user_id
+  end
+
   def current_user
     @user = (User.find_by(id: session[:user_id]) || User.new)
   end
