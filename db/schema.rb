7e52e0ff084ea34123f43c6a796f012c0867d19c
Christian Whitesides <whitesidescr@gmail.com>
2016-09-22 16:26:47 -0600

    Revert to Users table with only a name column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922222134) do
+ActiveRecord::Schema.define(version: 20160920221534) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -72,8 +72,7 @@ ActiveRecord::Schema.define(version: 20160922222134) do
     t.string   "uid",             null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
