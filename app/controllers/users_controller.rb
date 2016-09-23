f8a8adcb05989aa8a2a89de46aff34e4f12c9b9f
Christian Whitesides <whitesidescr@gmail.com>
2016-09-23 13:08:48 -0600

    Add set_user callback for easy set up among actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_logged_in, only: [:show]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -20,7 +20,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :provider, :uid, :password, :password_confirmation)
