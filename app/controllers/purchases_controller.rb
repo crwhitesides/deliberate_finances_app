22d1464343f376c85044e8984fee3f9d481242f2
Christian Whitesides <whitesidescr@gmail.com>
2016-09-29 16:18:16 -0600

    Finish code to add and edit plan purchases
    - Modify routes to strictly use nested routes
    - Continue tinkering with css

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,15 +3,32 @@ class PurchasesController < ApplicationController
   end
 
   def show
+    if params[:plan_id]
+      @plan = Plan.find_by(id: params[:plan_id])
+      @purchase = @plan.purchases.find_by(id: params[:id])
+      if @purchase.nil?
+        flash[:warning] = "Purchase item not found."
+        redirect_to current_user.current_plan
+      end
+    else
+      @purchase = Purchase.find(params[:id])
+    end
   end
 
   def new
-    @purchase = Purchase.new
+    if params[:plan_id] && !Plan.exists?(params[:plan_id])
+      flash[:warning] = "Plan not found."
+      redirect_to current_user.current_plan
+    else
+      plan = Plan.find_by(id: params[:plan_id])
+      @purchase = Purchase.new(plan_id: params[:plan_id])
+    end
   end
 
   def create
-    @purchase = current_user.current_plan.purchases.new(purchase_params)
+    @purchase = Purchase.new(purchase_params)
     if @purchase.save
+      flash[:success] = "Planned purchase added successfully."
       redirect_to current_user.current_plan
     else
       render 'new'
@@ -19,9 +36,31 @@ class PurchasesController < ApplicationController
   end
 
   def edit
+    if params[:plan_id]
+      plan = Plan.find_by(id: params[:plan_id])
+      if plan.nil?
+        flash[:warning] = "Plan not found."
+        redirect_to current_user.current_plan
+      else
+        @purchase = plan.purchases.find_by(id: params[:id])
+        if @purchase.nil?
+          flash[:warning] = "Purchase item not found."
+          redirect_to current_user.current_plan
+        end
+      end
+    else
+      @purchase = Purchase.find(params[:id])
+    end
   end
 
   def update
+    @purchase = Purchase.find(params[:id])
+    if @purchase.update(purchase_params)
+      flash[:success] = "Planned purchase updated successfully."
+      redirect_to current_user.current_plan
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -30,6 +69,6 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase).permit(:item, :price, :note)
+    params.require(:purchase).permit(:item, :price, :note, :plan_id)
   end
 end
