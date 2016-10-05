ba243856a6417cadb842a99896c30d77cbb2e943
Christian Whitesides <whitesidescr@gmail.com>
2016-10-05 14:54:21 -0600

    Update initializer and add columns to users table

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '9OkE8B5OrXUnqDQEba99nrP5j', '06l5Fhufhgh0gKmFNbvzNwcyDSLkG5dik2ip2FBa7GXB632F2m'
+  provider :google_oauth2, '654909388655-5a52hff9muk724utrctjc0sjcbbbia66.apps.googleusercontent.com', 'sAQ5cCRlCSQzoAzt1GOs2QCS', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004195059) do
+ActiveRecord::Schema.define(version: 20161005205031) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -71,10 +71,12 @@ ActiveRecord::Schema.define(version: 20161004195059) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "provider",        null: false
-    t.string   "uid",             null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "provider",         null: false
+    t.string   "uid",              null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
