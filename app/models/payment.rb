ac93b7d5289ff53b8a5923ccf29db3d5352914af
Christian Whitesides <whitesidescr@gmail.com>
2016-10-07 16:22:17 -0600

    Add class method to filter payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,4 +5,8 @@ class Payment < ActiveRecord::Base
   default_scope { order(created_at: :desc) }
   validates :amount, presence: true
   validates :store, presence: true, length: { maximum: 50 }
+
+  def self.filter_payments(userid)
+    where('user_id = ?', userid)
+  end
 end
