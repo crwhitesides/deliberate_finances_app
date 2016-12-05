d7d78987d0f01738b5b13db0859a3aa9cd92e149
Christian Whitesides <whitesidescr@gmail.com>
2016-12-05 15:43:52 -0700

    Move js from Plans show view to plans.js file

---
+++ app/assets/javascripts/plans.js
@@ -0,0 +1,55 @@
+$(function() {
+
+  function prettifyMonth(date) {
+    var rawDate = new Date(date);
+    var month = [];
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    return month[rawDate.getMonth()];
+  }
+
+
+  $(".js-previous").on("click", function() {
+    var nextId = parseInt($(".js-previous").attr("data-id")) - 1;
+    var numberOfPastPlans = parseInt($(".js-pager").attr("data-count")) - 1;
+    $.get("/plans/" + nextId + ".json", function(data) {
+      console.log(data)
+      $(".modal-title").text(`In ${prettifyMonth(data["date"])} you budgeted...`);
+
+      var purchases = data["purchases"];
+      var purchasesString = "";
+      $.each(purchases, function(i, purchase) {
+        purchasesString += `<p data-paymentid="${purchase["id"]}">` + "$" + `${purchase["price"]} ` + "for " + `${purchase["item"]} ` + "</p>";
+      });
+      $(".modal-body").html(purchasesString);
+    });
+    $(".js-pager").on("click", function() {
+      if (numberOfPastPlans === 0) {
+        $("#myModal").modal('hide');
+      } else {
+        numberOfPastPlans--;
+        nextId--;
+        $.get("/plans/" + nextId + ".json", function(data) {
+          $(".modal-title").text(`In ${prettifyMonth(data["date"])} you budgeted...`);
+          var purchases = data["purchases"];
+          var purchasesString = "";
+          $.each(purchases, function(i, purchase) {
+            purchasesString += `<p data-paymentid="${purchase["id"]}">` + "$" + `${purchase["price"]} ` + "for " + `${purchase["item"]} ` + "</p>";
+          });
+          $(".modal-body").html(purchasesString);
+        });
+      }
+    });
+  });
+});
