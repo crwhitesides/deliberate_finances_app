d01d61905b02ee49a6c2e3e464ad8a0e83e05c7c
Christian Whitesides <whitesidescr@gmail.com>
2016-12-05 16:36:39 -0700

    Write helper method to identity past plans from the reference of the current plan being viewed.

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -6,4 +6,12 @@ module PlansHelper
       plan.date.strftime("%B '%y")
     end
   end
+
+  def ids_of_past_plans(current_plan_id)
+    arr = current_user.past_plans.map do |plan|
+      plan.id
+    end
+    index_of_current_plan = arr.index(current_plan_id-1)
+    arr[0..index_of_current_plan]
+  end
 end
